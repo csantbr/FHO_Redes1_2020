a54790b274c930e261f3ff2d5568f2a8cda6cecb
csantbr <61211407+csantbr@users.noreply.github.com>
2020-10-29 21:14:30 -0200

    correção de indentação
    correção de indentação para melhor visibilidade do algoritmo

--- gravar_arquivo.c
+++ gravar_arquivo.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-int main ()
-{
-   FILE * pFile;
-   int n;
-   char name [100];
+int main () {
+    FILE * pFile;
+    int n;
+    char name[100];
 
-   pFile = fopen ("novo_arquivo.txt","w");
-   for (n=0 ; n<3 ; n++)
-   {
-     puts ("Digite seu nome: ");
-     gets (name);
-     fprintf (pFile, "Nome %d [%-10.10s]\n",n+1,name);
-   }
-   fclose (pFile);
+    pFile = fopen ("novo_arquivo.txt", "w");
+    for(n = 0; n < 3; n++) {
+		puts("Digite seu nome: ");
+		gets(name);
+		fprintf(pFile, "Nome %d [%-10.10s]\n", n+1, name);
+    }
+   
+    fclose(pFile);
 
-   return 0;
+    return 0;
 }
